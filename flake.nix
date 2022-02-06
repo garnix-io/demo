b30783ee8b88f734f6b0aaf94eea9c88eada7f67
Julian K. Arni <jkarni@gmail.com>
2022-02-06 16:08:44 +0100

    Add devShell with haskell basics

--- flake.nix
+++ flake.nix
@@ -5,7 +5,19 @@
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
+    packages.x86_64-linux.haskell = nixpkgs.legacyPackages.x86_64-linux.hello;
+
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
 
+    devShell.x86_64-linux =
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cabal2nix
+          cabal-install
+          hpack
+        ];
+      };
   };
 }
