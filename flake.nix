128d7345874be44f6a61897041aeeaf4b3cb6245
Julian K. Arni <jkarni@gmail.com>
2022-02-06 16:15:05 +0100

    Create haskell package

--- flake.nix
+++ flake.nix
@@ -1,17 +1,18 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.haskell = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.haskell = pkgs.haskellPackages.callPackage ./demo.nix { };
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
 
     devShell.x86_64-linux =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
       pkgs.mkShell {
         buildInputs = with pkgs; [
           cabal2nix
